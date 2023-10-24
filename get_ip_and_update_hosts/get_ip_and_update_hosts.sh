05fa0443ba62d794120be7098632017ba61669a1
João Rodrigues <97070413+Jaocodigos@users.noreply.github.com>
2023-10-23 22:44:58 -0300

    change on envs and letting file more dynamic

--- get_ip_and_update_hosts/get_ip_and_update_hosts.sh
+++ get_ip_and_update_hosts/get_ip_and_update_hosts.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
 # Caminho do arquivo de log
-OTP_FILE=$OTP_LOG_PATH
-
-CONNECT_FILE=$CONNECT_LOG_PATH
-
-HUB_FILE=$HUB_LOG_PATH
-
-RISK_FILE=$RISK_LOG_PATH
-
+service_file=$SERVICE_LOG_PATH
 
 # Nome do serviço para o qual você está monitorando o IP
-connect_service="connect.appliance.local"
-hub_service="hub.appliance.local"
-risk_service="risk.appliance.local"
-otp_service="otp.appliance.local"
-
+issuer=$SERVICE_ISSUER
 
 # Caminho do arquivo hosts
 hosts_file="/etc/hosts"
@@ -71,28 +60,16 @@ do
 
 
     # Obter o IP do último registro do log
-    otp_log_ip=$(get_ip_on_logs "$OTP_FILE")
-    connect_log_ip=$(get_ip_on_logs "$CONNECT_FILE")
-    risk_log_ip=$(get_ip_on_logs "$RISK_FILE")
-    hub_log_ip=$(get_ip_on_logs "$HUB_FILE")
+    service_log_ip=$(get_ip_on_logs "$service_file")
 
     # Verificando se os ips já existem no /etc/hosts
-    verify_if_already_exist "$connect_service" "$connect_log_ip"
-    verify_if_already_exist "$hub_service" "$hub_log_ip"
-    verify_if_already_exist "$risk_service" "$risk_log_ip"
-    verify_if_already_exist "$otp_service" "$otp_log_ip"
+    verify_if_already_exist "$issuer" "$service_log_ip"
 
     # Obter o IP atual do serviço no /etc/hosts
-    otp_on_hosts=$(grep -oP "(?<=\b$otp_service\b[[:space:]]+)[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" $hosts_file)
-    connect_on_hosts=$(grep -oP "(?<=\b$connect_service\b[[:space:]]+)[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" $hosts_file)
-    hub_on_hosts=$(grep -oP "(?<=\b$hub_service\b[[:space:]]+)[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" $hosts_file)
-    risk_on_hosts=$(grep -oP "(?<=\b$risk_service\b[[:space:]]+)[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" $hosts_file)
+    service_on_hosts=$(grep -oP "(?<=\b$issuer\b[[:space:]]+)[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" $hosts_file)
 
     # Mudar arquivo /etc/hosts se o ip tiver mudado
-    verify_ip_alteration "$connect_log_ip" "$connect_on_hosts" "$connect_service"
-    verify_ip_alteration "$hub_log_ip" "$hub_on_hosts" "$hub_service"
-    verify_ip_alteration "$risk_log_ip" "$risk_on_hosts" "$risk_service"
-    verify_ip_alteration "$otp_log_ip" "$otp_on_hosts" "$otp_service"
+    verify_ip_alteration "$service_log_ip" "$service_on_hosts" "$issuer"
 
     # Aguardar o intervalo de verificação antes de verificar novamente
     sleep $interval
